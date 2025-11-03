4e8e44a1b75a50b66fa5721f175eccd8cf6435f3
Leon Alhidayah <alhidayahtaufik010@gmail.com>
2025-11-03 11:07:53 +0700

    add hpi cpas dashboard

--- views/dashboard/project_hpi/dashboard_advertiser_hpi.py
+++ views/dashboard/project_hpi/dashboard_advertiser_hpi.py
@@ -1,20 +1,22 @@
 import streamlit as st
 
 from views.render_pages import (
+    display_advertiser_cpas_dashboard,
     display_advertiser_dashboard,
 )
+from views.style import load_css
 
 st.set_page_config(layout="wide")
 PROJECT_NAME = "HPI"
 
 st.header(f"Dashboard Advertiser Marketplace {PROJECT_NAME}")
 
-# load_css()
+load_css()
 
-# mp_tab, cpas_tab = st.tabs(["Marketplace", "CPAS"])
+mp_tab, cpas_tab = st.tabs(["Marketplace", "CPAS"])
 
-# with mp_tab:
-display_advertiser_dashboard(project_name=PROJECT_NAME)
+with mp_tab:
+    display_advertiser_dashboard(project_name=PROJECT_NAME)
 
-# with cpas_tab:
-#     display_advertiser_cpas_dashboard(project_name=PROJECT_NAME)
+with cpas_tab:
+    display_advertiser_cpas_dashboard(project_name=PROJECT_NAME)
